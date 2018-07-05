bbeee6ff669db4ea90a6ebc6d4afd4d9a95d0d93
nick72ali <nick72ali@gmail.com>
2018-07-04 22:49:36 -0400

    Change 2
    Added ground script
    Changed player control to use axis translation instead of physics

---
+++ Sidescroller V1/Assets/Scripts/GroundScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    //If the player is on the ground, it will assign a variable for use in the jump control
+    public void OnCollisionEnter(Collision PlayerTouch)
+    {
+
+       int PlayerOnGround = 1;
+        Debug.Log("Player is on ground");
+
+    }
+    public void OnCollisionExit(Collision PlayerTouch)
+    {
+
+        int PlayerOnGround = 0;
+        Debug.Log("Player is off the ground");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Sidescroller V1/Assets/Scripts/PlayerControlScript.cs
+++ Sidescroller V1/Assets/Scripts/PlayerControlScript.cs
@@ -4,26 +4,47 @@ using UnityEngine;
 
 public class PlayerControlScript : MonoBehaviour {
 
-    Rigidbody Player;
+   
+    Vector3 PlayerPos;
 	// Use this for initialization
 	void Start () {
-        Player = GetComponent<Rigidbody>();
+        
        
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        int PlayerSpeed = 5;
-        //while A key is pressed
+
+        //Set Player's speed and jump height
+        float PlayerSpeed = 0.05f;
+        float PlayerJumpHt = 5;
+
+        //while A key is pressed, move left
         if (Input.GetKey(KeyCode.A))
         {
-            Player.velocity = new Vector3(-PlayerSpeed, 0, 0);
-        }
+          
+            PlayerPos = transform.position;
+            PlayerPos.x -= PlayerSpeed;
+            transform.position = PlayerPos;
+            Debug.Log("Player moving left");
 
-        //while D key is pressed
+        }
+        //while D key is pressed, move right
         if (Input.GetKey(KeyCode.D))
         {
-            Player.velocity = new Vector3(PlayerSpeed, 0, 0);
+            
+            PlayerPos = transform.position;
+            PlayerPos.x += PlayerSpeed;
+            transform.position = PlayerPos;
+            Debug.Log("Player moving right");
+
+        }
+        //while W key is pressed and player is on the ground, jump
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            
+            Debug.Log("Player jumping");
+
         }
 	}
 }
