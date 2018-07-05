3f83361a69541e17bedddc9aa47a1b0d151bb429
nick72ali <nick72ali@gmail.com>
2018-07-04 20:38:37 -0400

    Movement bug

---
+++ Sidescroller V1/Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour {
+
+    Vector3 PlayerPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //while A key is pressed
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlayerPos = transform.position;
+            PlayerPos.x -= 0.5f;
+            transform.position = PlayerPos;
+        }
+
+        //while D key is pressed
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            PlayerPos = transform.position;
+            PlayerPos.x += 0.5f;
+            transform.position = PlayerPos;
+        }
+	}
+}
