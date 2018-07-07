5bcc8ef0aa2fe96d03a72ebae78e0f5ab8210c15
nick72ali <nick72ali@gmail.com>
2018-07-07 11:55:31 -0400

    Change 9
    Animations are extremely buggy, tried to rig them with code, didn't work. May need to try a new method.

---
+++ Sidescroller V1/Assets/Scripts/PlayerAnimationScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationScript : MonoBehaviour {
+
+    public GameObject PlayerIdle;
+    public GameObject PlayerWalk;
+    public GameObject PlayerSpellCast;
+    public GameObject PlayerSpellCharge;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+
+            PlayerIdle.SetActive(true);
+            PlayerWalk.SetActive(false);
+            PlayerSpellCast.SetActive(false);
+            PlayerSpellCharge.SetActive(false);
+        
+		
+        if (Input.GetKey(KeyCode.D))
+        {
+            PlayerIdle.SetActive(false);
+            PlayerWalk.SetActive(true);
+            PlayerSpellCast.SetActive(false);
+            PlayerSpellCharge.SetActive(false);
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            PlayerIdle.SetActive(false);
+            PlayerWalk.SetActive(false);
+            PlayerSpellCast.SetActive(false);
+            PlayerSpellCharge.SetActive(true);
+        }
+	}
+}
