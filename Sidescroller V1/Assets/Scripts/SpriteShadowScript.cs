95d0b137163962355ac0490f2365225af6e67c88
nick72ali <nick72ali@gmail.com>
2018-07-09 01:21:17 -0400

    stuff

---
+++ Sidescroller V1/Assets/Scripts/SpriteShadowScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteShadowScript : MonoBehaviour {
+    public Material mat;
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        
+    }
+}
