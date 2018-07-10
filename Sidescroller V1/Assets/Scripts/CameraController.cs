0018b9cf8aeaaa4096854093e47a0fdec0eb7463
Count Grimhart <grimmcrypt@live.com>
2018-07-10 19:38:07 -0400

    Smoothed Out camera,

--- Sidescroller V1/Assets/Scripts/CameraController.cs
+++ Sidescroller V1/Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//Script is used to keep camera focused on the player
+/*Script is used to keep camera focused on the player
 public class CameraController : MonoBehaviour {
 
 
@@ -20,4 +20,24 @@ public class CameraController : MonoBehaviour {
     {
         transform.position = Player.transform.position + Offset;
     }
+}*/
+
+//Learned With Brackeys Video
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPosition;
+
+        //transform.LookAt(target);
+    }
+
 }
